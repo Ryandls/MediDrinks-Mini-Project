d6f684a3e7b3b88e680878d08820c047a2e6a766
Bryan Dos Santos <bryandouglasxd@gmail.com>
2021-04-01 23:27:00 -0300

    Revert "Revert "refactoring""
    This reverts commit 3687bb9e55cfc56c11997596e7dbfacfb8cae3f0.

--- src/components/mainBody.vue
+++ src/components/mainBody.vue
@@ -1,30 +1,32 @@
-<template >
-    <div class="mainBody">
-            <h1>Os melhores drinks não alcoólicos do <span class="textBlue">Brasil</span></h1>
-            <p>
-            <p>50 resultados encontrados</p>
-    </div>
-    
+<template>
+  <div class="searcStatus">
+    <h1>
+      Os melhores drinks não alcoólicos do <span class="textBlue">Brasil</span>
+    </h1>
+    <p></p>
+    <p>Foram encontrados {{ found }} resultados.</p>
+  </div>
 </template>
+
 <script>
 
-    
 export default {
-}
+  name: "DrinkSearchStatus",
+  props: ["found"],
+};
+
 </script>
+
 <style>
-    .mainBody {
 
-        width: 100vw;
-        justify-content: center;
-        padding: 2rem 5rem 5rem 3rem;
+.mainBody {
+  width: 100vw;
+  justify-content: center;
+  padding: 2rem 5rem 5rem 3rem;
+}
 
-    }
-
-
-    .textBlue {
-        color:blue;
-        
-    }
+.textBlue {
+  color: blue;
+}
 
 </style>
