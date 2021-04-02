7063506b3876acf70b2cdb6bf3105c0a686502fa
Bryan Dos Santos <bryandouglasxd@gmail.com>
2021-04-01 23:25:21 -0300

    Revert "refactoring"
    This reverts commit c83c77fa4588384322026594c18c8ce9027c2276.

--- src/components/bodyTable.vue
+++ src/components/bodyTable.vue
@@ -1,36 +1,49 @@
 <template>
-  <div class="mainBody">
-    
-    <h1>  Os melhores drinks não alcoólicos do <span class="textBlue">Brasil</span></h1>
-    
-    <p>{{found }} resultados encontrados.</p>
-  </div>
+        <div class="card">
+            <th  v-for="Drink of drinks" :key="Drink.idDrink">
+                <td>
+                    <ul><img v-bind:src="Drink.strDrinkThumb"></ul>
+                    <ul>{{Drink.strDrink}}</ul>
+                    <ul>{{Drink.idDrink}}</ul>
+                </td>
+            </th>
+        </div>
 </template>
-
 <script>
-
+import Drink from '../services/Drinks.js'
+    
 export default {
-  name: "DrinkSearchStatus",
-  props: ["found"],
-};
 
+    data(){
+        return {
+            drinks:[]
+        }
+        
+    },
+
+     mounted(){
+         Drink.listar().then(res => {
+             const allDrinks = res.data.length
+             this.drinks = res.data.drinks;
+             return allDrinks;
+
+         })
+     }, 
+}
 </script>
-
-<style scoped>
-
-.mainBody {
-  width: 100vw;
-  padding: 1rem 2rem 2rem 4rem;
+<style>
+.card{
+    width: 310px;
+    height: 394px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    
 }
+img{
+    width: 310px;
+    height: 280px;
 
-h1 {
-    font-size: 24px;
-}
-p{
-    color:grey;
-}
-.textBlue {
-  color: blue;
+    margin-left:5px;
 }
 
 </style>
